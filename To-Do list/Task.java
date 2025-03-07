931cc2168c7b5298cde548c29a71f5148f24489a
aditii67 <aditikulam@gmail.com>
2025-03-07 21:30:44 +0530

    Create Task.java

---
+++ To-Do list/Task.java
@@ -0,0 +1,27 @@
+// Represents a single task
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markCompleted() {
+        isCompleted = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return (isCompleted ? "[✔] " : "[ ] ") + description;
+    }
+}
